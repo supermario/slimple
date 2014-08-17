819408827cef074592d4e616bc999d01e655170c
Mario Rogic <hello@mario.net.au>
2014-08-17 21:10:20 +1000

    Basic stripped down starting point

---
+++ Procfile
@@ -0,0 +1,2 @@
+# http://phlippers.net/blog/2012/07/03/improved-concurrency-for-heroku-dynos/
+web: bundle exec thin -p $PORT -e $RACK_ENV start
---
+++ app.rb
@@ -0,0 +1,24 @@
+﻿require 'rubygems'
+require 'sinatra'
+require 'slim'
+require 'less'
+
+Slim::Engine.set_default_options pretty: true, tabsize: 2
+
+get '/' do
+  @tpl = :page
+  slim @tpl
+end
+
+get '/css/*.less' do
+  filename = params[:splat].first
+  # parse our output with less!
+  less filename.to_sym
+end
+
+not_found do
+  halt 404, 'Page not found'
+end
+
+helpers do
+end
